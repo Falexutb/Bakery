d5b2ea0fbece5c7edc9ac5b362ee776c15e50a97
Falexutb <159246855+Falexutb@users.noreply.github.com>
2024-03-01 23:40:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Placeholder function for subscription (you may replace it with your actual logic)
+function subscribe() {
+    alert('Subscription functionality will be implemented here.');
+}
